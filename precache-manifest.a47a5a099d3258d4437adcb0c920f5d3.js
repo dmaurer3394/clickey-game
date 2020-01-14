4bec22da90372f47754d877765618c8f3cb9ccc5
Dalton Maurer <dmaurer3394@gmail.com>
2020-01-14 13:34:06 -0500

    Updates

---
+++ precache-manifest.a47a5a099d3258d4437adcb0c920f5d3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf786c5d70c757b0650bc47cffcdf575",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "12c678b719565c69637c",
+    "url": "/clicky-game/static/css/main.7aaa14d5.chunk.css"
+  },
+  {
+    "revision": "ed41e2c259a20899e63b",
+    "url": "/clicky-game/static/js/2.7ed9e50a.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/clicky-game/static/js/2.7ed9e50a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "12c678b719565c69637c",
+    "url": "/clicky-game/static/js/main.004bf50d.chunk.js"
+  },
+  {
+    "revision": "b5e3d5480cb174bedaf8",
+    "url": "/clicky-game/static/js/runtime-main.dbb64346.js"
+  }
+]);
